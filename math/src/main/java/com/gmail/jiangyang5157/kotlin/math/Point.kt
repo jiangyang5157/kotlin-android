a329bab80adaa202893122abd337d54b9220646c
yangjiang <jiangyang5157@gmail.com>
2017-06-26 22:19:46 +1200

    data class Point

--- math/src/main/java/com/gmail/jiangyang5157/kotlin/math/Point.kt
+++ math/src/main/java/com/gmail/jiangyang5157/kotlin/math/Point.kt
@@ -3,6 +3,24 @@ package com.gmail.jiangyang5157.kotlin.math
 /**
  * Created by Yang Jiang on June 26, 2017
  */
-data class PointF(val x: Float, val y: Float)
+data class Point(val x: Double, val y: Double) {
 
-// TODO
+    operator fun unaryMinus(): Point = Point(-x, -y)
+
+    operator fun plus(other: Point): Point = Point(x + other.x, y + other.y)
+    operator fun plus(other: Int): Point = Point(x + other, y + other)
+    operator fun plus(other: Double): Point = Point(x + other, y + other)
+
+    operator fun minus(other: Point): Point = Point(x - other.x, y - other.y)
+    operator fun minus(other: Int): Point = Point(x - other, y - other)
+    operator fun minus(other: Double): Point = Point(x - other, y - other)
+
+    operator fun times(other: Point): Point = Point(x * other.x, y * other.y)
+    operator fun times(other: Int): Point = Point(x * other, y * other)
+    operator fun times(other: Double): Point = Point(x * other, y * other)
+
+    operator fun div(other: Point): Point = Point(x / other.x, y / other.y)
+    operator fun div(other: Int): Point = Point(x / other, y / other)
+    operator fun div(other: Double): Point = Point(x / other, y / other)
+
+}
--- math/src/test/kotlin/com/gmail/jiangyang5157/kotlin/math/PointTest.kt
+++ math/src/test/kotlin/com/gmail/jiangyang5157/kotlin/math/PointTest.kt
@@ -1,7 +1,7 @@
 package com.gmail.jiangyang5157.kotlin.math
 
 import org.junit.Test
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
 
 /**
  * Created by Yang Jiang on June 26, 2017
@@ -9,10 +9,27 @@ import kotlin.test.assertTrue
 class PointTest {
 
     @Test
-    fun testsWork() {
-        val p = PointF(-1F,2F)
-        println(p)
-        assertTrue(true)
+    fun test_overrider_operators() {
+        val a = Point(10.0, 10.0)
+        val b = Point(-2.0, 2.0)
+
+        assertEquals(Point(2.0, -2.0), -b)
+
+        assertEquals(Point(8.0, 12.0), a + b)
+        assertEquals(Point(30.0, 30.0), a + 20)
+        assertEquals(Point(30.2, 30.2), a + 20.2)
+
+        assertEquals(Point(12.0, 8.0), a - b)
+        assertEquals(Point(-10.0, -10.0), a - 20)
+        assertEquals(Point(-10.2, -10.2), a - 20.2)
+
+        assertEquals(Point(-20.0, 20.0), a * b)
+        assertEquals(Point(200.0, 200.0), a * 20)
+        assertEquals(Point(202.0, 202.0), a * 20.2)
+
+        assertEquals(Point(-5.0, 5.0), a / b)
+        assertEquals(Point(0.5, 0.5), a / 20)
+        assertEquals(Point(1.0, 1.0), a / 10)
     }
 
 }
