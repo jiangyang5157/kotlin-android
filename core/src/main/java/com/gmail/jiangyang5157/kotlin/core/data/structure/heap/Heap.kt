9f460981501e261826af919ce604fbbf6a366d3c
yangjiang <jiangyang5157@gmail.com>
2017-06-28 22:12:11 +1200

    remove pkg heap, tree

--- core/src/main/java/com/gmail/jiangyang5157/kotlin/core/data/structure/heap/Heap.kt
+++
@@ -1,6 +0,0 @@
-package com.gmail.jiangyang5157.kotlin.core.data.structure.heap
-
-/**
- * Created by Yang Jiang on June 28, 2017
- */
-// TODO
--- core/src/main/java/com/gmail/jiangyang5157/kotlin/core/data/structure/tree/Bst.kt
+++
@@ -1,6 +0,0 @@
-package com.gmail.jiangyang5157.kotlin.core.data.structure.tree
-
-/**
- * Created by Yang Jiang on June 28, 2017
- */
-// TODO
