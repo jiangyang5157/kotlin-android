514e9eb188e3754dd1f86e05411280f4b68bab4e
yangjiang <jiangyang5157@gmail.com>
2017-06-29 01:16:08 +1200

    remove override operator for Point

--- math/src/main/java/com/gmail/jiangyang5157/kotlin/math/Point2.kt
+++ math/src/main/java/com/gmail/jiangyang5157/kotlin/math/Point2.kt
@@ -10,20 +10,4 @@ data class Point2(val x: Double, val y: Double) {
 
     operator fun unaryMinus(): Point2 = Point2(-x, -y)
 
-    operator fun plus(other: Point2): Point2 = Point2(x + other.x, y + other.y)
-    operator fun plus(int: Int): Point2 = Point2(x + int, y + int)
-    operator fun plus(double: Double): Point2 = Point2(x + double, y + double)
-
-    operator fun minus(other: Point2): Point2 = Point2(x - other.x, y - other.y)
-    operator fun minus(int: Int): Point2 = Point2(x - int, y - int)
-    operator fun minus(double: Double): Point2 = Point2(x - double, y - double)
-
-    operator fun times(other: Point2): Point2 = Point2(x * other.x, y * other.y)
-    operator fun times(int: Int): Point2 = Point2(x * int, y * int)
-    operator fun times(double: Double): Point2 = Point2(x * double, y * double)
-
-    operator fun div(other: Point2): Point2 = Point2(x / other.x, y / other.y)
-    operator fun div(int: Int): Point2 = Point2(x / int, y / int)
-    operator fun div(double: Double): Point2 = Point2(x / double, y / double)
-
 }
--- math/src/test/java/com/gmail/jiangyang5157/kotlin/math/Point3Test.kt
+++ math/src/test/java/com/gmail/jiangyang5157/kotlin/math/Point3Test.kt
@@ -1,8 +1,5 @@
 package com.gmail.jiangyang5157.kotlin.math
 
-import org.junit.Test
-import kotlin.test.assertEquals
-
 /**
  * Created by Yang Jiang on June 26, 2017
  */
@@ -24,30 +21,6 @@ class Point3Test {
         assertEquals(1.0, threeVal.x)
         assertEquals(2.0, threeVal.y)
         assertEquals(3.0, threeVal.z)
-    }
-
-    @Test
-    fun test_overrider_operator() {
-        val a = Point3(10.0, 10.0, 10.0)
-        val b = Point3(-2.0, 2.0, 1.0)
-
-        assertEquals(Point3(2.0, -2.0, -1.0), -b)
-
-        assertEquals(Point3(8.0, 12.0, 11.0), a + b)
-        assertEquals(Point3(30.0, 30.0, 30.0), a + 20)
-        assertEquals(Point3(30.2, 30.2, 30.2), a + 20.2)
-
-        assertEquals(Point3(12.0, 8.0, 9.0), a - b)
-        assertEquals(Point3(-10.0, -10.0, -10.0), a - 20)
-        assertEquals(Point3(-10.2, -10.2, -10.2), a - 20.2)
-
-        assertEquals(Point3(-20.0, 20.0, 10.0), a * b)
-        assertEquals(Point3(200.0, 200.0, 200.0), a * 20)
-        assertEquals(Point3(202.0, 202.0, 202.0), a * 20.2)
-
-        assertEquals(Point3(-5.0, 5.0, 10.0), a / b)
-        assertEquals(Point3(0.5, 0.5, 0.5), a / 20)
-        assertEquals(Point3(1.0, 1.0, 1.0), a / 10)
     }
 
 }
