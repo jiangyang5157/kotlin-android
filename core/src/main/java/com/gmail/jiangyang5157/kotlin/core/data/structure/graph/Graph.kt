0b6882f6f9c95e94989a9cd2117b63a76f70d29d
Yang Jiang <yang.jiang@fiserv.com>
2017-06-30 15:44:36 +1200

    na

--- core/src/main/java/com/gmail/jiangyang5157/kotlin/core/data/structure/graph/Graph.kt
+++ core/src/main/java/com/gmail/jiangyang5157/kotlin/core/data/structure/graph/Graph.kt
@@ -8,34 +8,57 @@ open class Graph<T> {
     /**
      * nodes HashMap<Node.id, Node> stores all nodes.
      */
-    val nodes: HashMap<T, Node<T>> = HashMap<T, Node<T>>()
+    val nodes: HashMap<T, Node<T>> = HashMap()
 
     /**
      * sources HashMap<Node.id, HashMap<Node.id, Edge>>  maps a node to parents with edge.
      */
-    val sources: HashMap<T, HashMap<T, Edge>> = HashMap<T, HashMap<T, Edge>>()
+    val sources: HashMap<T, HashMap<T, Edge>> = HashMap()
 
     /**
      * targets HashMap<Node.id, HashMap<Node.id, Edge>>  maps a node to children with edge.
      */
-    val targets: HashMap<T, HashMap<T, Edge>> = HashMap<T, HashMap<T, Edge>>()
+    val targets: HashMap<T, HashMap<T, Edge>> = HashMap()
 
-    fun getNode(id : T): Node<T>? {
+    fun getNode(id: T): Node<T>? {
         return nodes[id]
     }
 
-    fun getSource(id : T): HashMap<T, Edge>? {
+    fun getSource(id: T): HashMap<T, Edge>? {
         return sources[id]
     }
 
-    fun getTargets(id : T): HashMap<T, Edge>? {
+    fun getTargets(id: T): HashMap<T, Edge>? {
         return targets[id]
     }
 
-    fun GetEdge(src: T, tgt: T): Edge? {
-        val fromSrc = targets[src] ?: return null
-        val edge = fromSrc[tgt] ?: return null
-        return edge
+    fun getEdge(src: T, tgt: T): Edge? {
+        val children = targets[src] ?: return null
+        return children[tgt]
+    }
+
+    fun addNode(node: Node<T>) {
+        nodes.put(node.id, node)
+    }
+
+    fun updateNode(node: Node<T>) {
+        nodes.replace(node.id, node)
+    }
+
+    fun deleteNode(id: T) {
+        nodes.remove(id)
+        sources.remove(id)
+        targets.remove(id)
+
+        // Remove edges which source is the node
+        sources.forEach { _, parents ->
+            parents.remove(id)
+        }
+
+        // remove edges which target is the node
+        targets.forEach { _, children ->
+            children.remove(id)
+        }
     }
 
 }
