bfec05445abe4c89a91f7da5abb4c9231ad3ab26
yangjiang <jiangyang5157@gmail.com>
2017-07-16 18:29:26 +1200

    instance of Fragment

---
+++ kotlin-android-core/src/main/java/com/gmail/jiangyang5157/kotlin_android_core/utils/FragmentUtils.kt
@@ -0,0 +1,12 @@
+package com.gmail.jiangyang5157.kotlin_android_core.utils
+
+import android.app.Fragment
+import android.os.Bundle
+
+/**
+ * Created by Yang Jiang on July 16, 2017
+ */
+inline fun <reified T : Fragment> instance(args: Bundle? = null): Fragment
+        = T::class.java.newInstance().apply {
+    arguments = args
+}
