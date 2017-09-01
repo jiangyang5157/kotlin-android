a58c67de6a21906ed345a273ae5c5598b64d1e71
yangjiang <jiangyang5157@gmail.com>
2017-09-01 22:18:17 +1200

    test flipview

---
+++ app/src/main/java/com/gmail/jiangyang5157/kotlin/FlipViewActivity.kt
@@ -0,0 +1,28 @@
+package com.gmail.jiangyang5157.kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageView
+import com.gmail.jiangyang5157.kotlin_android_flipview.FlipView
+
+/**
+ * Created by Yang Jiang on September 01, 2017
+ */
+class FlipViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flipview)
+
+        val flipView = findViewById(R.id.fv) as FlipView
+        val frontImg = ImageView(this)
+        val backImg = ImageView(this)
+        frontImg.setImageDrawable(getDrawable(R.mipmap.ic_launcher))
+        backImg.setImageDrawable(getDrawable(R.mipmap.ic_launcher_round))
+        flipView.reset(frontImg, backImg, FlipView.Direction.BOTTOM_IN_TOP_OUT)
+
+        val btnFlip = findViewById(R.id.btn_flip) as Button
+        btnFlip.setOnClickListener { flipView.commitFlip() }
+    }
+}
--- app/src/main/java/com/gmail/jiangyang5157/kotlin/MainActivity.kt
+++ app/src/main/java/com/gmail/jiangyang5157/kotlin/MainActivity.kt
@@ -1,12 +1,53 @@
 package com.gmail.jiangyang5157.kotlin
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rvModules = findViewById(R.id.rv_modules) as RecyclerView
+        rvModules.setHasFixedSize(true)
+        rvModules.layoutManager = LinearLayoutManager(this)
+        rvModules.adapter = ModuleAdapter()
+    }
+
+    class ModuleViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    class ModuleAdapter : RecyclerView.Adapter<ModuleViewHolder>() {
+
+        private val mModules = arrayOf(
+                "Flip View"
+        )
+
+        private val mModuleOnClickListener = View.OnClickListener { v ->
+            when {
+                v.tag == "Flip View" -> v.context.startActivity(Intent(v.context, FlipViewActivity::class.java))
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return mModules.size
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ModuleViewHolder {
+            return ModuleViewHolder(TextView(parent?.context))
+        }
+
+        override fun onBindViewHolder(holder: ModuleViewHolder?, position: Int) {
+            with(holder?.itemView as TextView) {
+                text = mModules[position]
+                tag = mModules[position]
+                setOnClickListener(mModuleOnClickListener)
+            }
+        }
     }
 }
