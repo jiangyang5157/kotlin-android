3f762204b64763099f478e643b8d9305acce7f6d
yangjiang <jiangyang5157@gmail.com>
2018-04-13 22:12:41 +1200

    init injector module

---
+++ kotlin-android-injector/src/main/java/com/gmail/jiangyang5157/kotlin_android_injector/FeatureController.kt
@@ -0,0 +1,11 @@
+package com.gmail.jiangyang5157.kotlin_android_injector
+
+/**
+ * Created by Yang Jiang on April 13, 2018
+ */
+interface FeatureController {
+
+    fun start()
+
+    fun stop()
+}
---
+++ kotlin-android-injector/src/main/java/com/gmail/jiangyang5157/kotlin_android_injector/FeatureInjector.java
@@ -0,0 +1,9 @@
+package com.gmail.jiangyang5157.kotlin_android_injector;
+
+/**
+ * Created by Yang Jiang on April 13, 2018
+ */
+public class FeatureInjector {
+
+
+}
