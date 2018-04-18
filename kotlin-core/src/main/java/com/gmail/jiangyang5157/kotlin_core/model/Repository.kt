9ad3b81a20dcd9b67c2e6830b507e83ade7c99d6
Yang Jiang <yang.jiang@fiserv.com>
2018-04-19 10:38:20 +1200

    na

--- kotlin-core/src/main/java/com/gmail/jiangyang5157/kotlin_core/model/Repository.kt
+++ kotlin-core/src/main/java/com/gmail/jiangyang5157/kotlin_core/model/Repository.kt
@@ -3,17 +3,17 @@ package com.gmail.jiangyang5157.kotlin_core.model
 /**
  * Created by Yang Jiang on April 18, 2018
  */
-interface Repository<T, in Specification> {
+interface Repository<T, in S> {
 
     fun add(item: T): Boolean
 
     fun add(items: Iterable<T>): Boolean
 
-    fun find(spec: Specification): List<T>
+    fun find(spec: S): List<T>
 
-    fun remove(spec: Specification): Boolean
+    fun remove(spec: S): Boolean
 
-    fun update(item: T, spec: Specification): Boolean
+    fun update(item: T, spec: S): Boolean
 
 }
 
