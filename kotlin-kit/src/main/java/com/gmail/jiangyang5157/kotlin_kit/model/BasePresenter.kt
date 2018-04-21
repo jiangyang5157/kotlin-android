de0e83fcfb71970caf8d42b3a9a8d41494a38a7d
yangjiang <jiangyang5157@gmail.com>
2018-04-21 19:24:17 +1200

    doc comments

---
+++ kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/model/BasePresenter.kt
@@ -0,0 +1,6 @@
+package com.gmail.jiangyang5157.kotlin_kit.model
+
+/**
+ * Created by Yang Jiang on April 21, 2018
+ */
+interface BasePresenter
--- kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/model/BaseView.kt
+++ kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/model/BaseView.kt
@@ -3,7 +3,7 @@ package com.gmail.jiangyang5157.kotlin_kit.model
 /**
  * Created by Yang Jiang on April 18, 2018
  */
-interface BaseView<in T> {
+interface BaseView<in T : BasePresenter> {
 
     fun setPresenter(presenter: T)
 
