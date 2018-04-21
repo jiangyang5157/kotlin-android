783721a6801062bdc876bacf81ca8929ca9b506a
yangjiang <jiangyang5157@gmail.com>
2018-04-21 19:06:51 +1200

    doc comments

--- kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/model/BaseView.kt
+++ kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/model/BaseView.kt
@@ -3,7 +3,7 @@ package com.gmail.jiangyang5157.kotlin_kit.model
 /**
  * Created by Yang Jiang on April 18, 2018
  */
-interface BaseView<in T : BasePresenter> {
+interface BaseView<in T> {
 
     fun setPresenter(presenter: T)
 
--- kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/model/Repository.kt
+++ kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/model/Repository.kt
@@ -3,17 +3,17 @@ package com.gmail.jiangyang5157.kotlin_kit.model
 /**
  * Created by Yang Jiang on April 18, 2018
  */
-interface Repository<T, in S> {
+interface Repository<T, in Specification> {
 
     fun add(item: T): Boolean
 
     fun add(items: Iterable<T>): Boolean
 
-    fun find(spec: S): List<T>
+    fun find(spec: Specification): List<T>
 
-    fun remove(spec: S): Boolean
+    fun remove(spec: Specification): Boolean
 
-    fun update(item: T, spec: S): Boolean
+    fun update(item: T, spec: Specification): Boolean
 
 }
 
