e3d5f4027b7dd8fe496e33a424952d10e0b19fbd
yangjiang <jiangyang5157@gmail.com>
2018-04-24 01:10:03 +1200

    Ext.

--- kotlin-android-kit/src/main/java/com/gmail/jiangyang5157/kotlin_android_kit/ext/FragmentExt.kt
+++ kotlin-android-kit/src/main/java/com/gmail/jiangyang5157/kotlin_android_kit/ext/FragmentExt.kt
@@ -7,6 +7,4 @@ import android.support.v4.app.Fragment
  * Created by Yang Jiang on July 16, 2017
  */
 
-inline fun <reified T : Fragment> instance(args: Bundle? = null): Fragment = T::class.java.newInstance().apply {
-    arguments = args
-}
+inline fun <reified T : Fragment> instance(args: Bundle? = null): Fragment = T::class.java.newInstance().apply { arguments = args }
--- kotlin-android-kit/src/main/java/com/gmail/jiangyang5157/kotlin_android_kit/ext/GlobeExt.kt
+++
@@ -1,9 +0,0 @@
-package com.gmail.jiangyang5157.kotlin_android_kit.ext
-
-import android.os.Build
-
-/**
- * Created by Yang Jiang on April 24, 2018
- */
-
-fun sdkValidate(sdkVersion: Int): Boolean = Build.VERSION.SDK_INT >= sdkVersion
