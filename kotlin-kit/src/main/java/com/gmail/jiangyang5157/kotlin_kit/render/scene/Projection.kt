a98711539f7e6868b1323be67fbe2cb8d6e5bd1c
yangjiang <jiangyang5157@gmail.com>
2018-04-25 14:21:34 +1200

    scene

--- kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/render/scene/Projection.kt
+++ kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/render/scene/Projection.kt
@@ -5,8 +5,8 @@ import com.gmail.jiangyang5157.kotlin_kit.render.scene.shape.Shape
 /**
  * Created by Yang Jiang on April 25, 2018
  */
-interface Projection<in S : Shape, in Texture> {
+interface Projection<in S : Shape, in Specification, in Texture> {
 
-    fun map(shape: S, texture: Texture)
+    fun map(shape: S, spec: Specification, texture: Texture)
 
 }
