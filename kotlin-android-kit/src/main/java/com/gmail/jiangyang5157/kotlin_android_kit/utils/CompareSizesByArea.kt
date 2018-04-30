e3ad429baa460a6102f46268c242b6206fe40da9
yangjiang <jiangyang5157@gmail.com>
2018-05-01 00:46:21 +1200

    CompareSizesByArea

---
+++ kotlin-android-kit/src/main/java/com/gmail/jiangyang5157/kotlin_android_kit/utils/CompareSizesByArea.kt
@@ -0,0 +1,18 @@
+package com.gmail.jiangyang5157.kotlin_android_kit.utils
+
+import android.os.Build
+import android.support.annotation.RequiresApi
+import android.util.Size
+import java.lang.Long.signum
+
+/**
+ * Created by Yang Jiang on April 30, 2018
+ */
+internal class CompareSizesByArea : Comparator<Size> {
+
+    // We cast here to ensure the multiplications won't overflow
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun compare(lhs: Size, rhs: Size) =
+            signum(lhs.width.toLong() * lhs.height - rhs.width.toLong() * rhs.height)
+
+}
