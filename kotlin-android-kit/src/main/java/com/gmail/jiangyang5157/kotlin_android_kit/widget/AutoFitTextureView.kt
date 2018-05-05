1151805a53737ade8029acc88a1b3d884cf0fc12
yangjiang <jiangyang5157@gmail.com>
2018-05-06 01:07:47 +1200

    CompareSizesByArea

---
+++ kotlin-android-kit/src/main/java/com/gmail/jiangyang5157/kotlin_android_kit/widget/AutoFitTextureView.kt
@@ -0,0 +1,54 @@
+package com.gmail.jiangyang5157.kotlin_android_kit.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.TextureView
+import android.view.View
+
+/**
+ * A [TextureView] that can be adjusted to a specified aspect ratio.
+ */
+class AutoFitTextureView : TextureView {
+
+    private var ratioWidth = 0
+
+    private var ratioHeight = 0
+
+    constructor(context: Context?)
+            : super(context)
+
+    constructor(context: Context?, attrs: AttributeSet?)
+            : super(context, attrs)
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int)
+            : super(context, attrs, defStyleAttr)
+
+    /**
+     * @param width  Relative horizontal size
+     * @param height Relative vertical size
+     */
+    fun setAspectRatio(width: Int, height: Int) {
+        if (width < 0 || height < 0) {
+            throw IllegalArgumentException("Size cannot be negative.")
+        }
+        ratioWidth = width
+        ratioHeight = height
+        requestLayout()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = View.MeasureSpec.getSize(widthMeasureSpec)
+        val height = View.MeasureSpec.getSize(heightMeasureSpec)
+        
+        if (0 == ratioWidth || 0 == ratioHeight) {
+            setMeasuredDimension(width, height)
+        } else {
+            if (width < height * ratioWidth / ratioHeight) {
+                setMeasuredDimension(width, width * ratioHeight / ratioWidth)
+            } else {
+                setMeasuredDimension(height * ratioWidth / ratioHeight, height)
+            }
+        }
+    }
+}
---
+++ kotlin-android-kit/src/main/java/com/gmail/jiangyang5157/kotlin_android_kit/widget/SquareFrameLayout.kt
@@ -0,0 +1,33 @@
+package com.gmail.jiangyang5157.kotlin_android_kit.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+
+/**
+ * Created by Yang Jiang on April 21, 2018
+ */
+class SquareFrameLayout : FrameLayout {
+
+    constructor(context: Context?)
+            : super(context)
+
+    constructor(context: Context?, attrs: AttributeSet?)
+            : super(context, attrs)
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int)
+            : super(context, attrs, defStyleAttr)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+        val width = View.MeasureSpec.getSize(widthMeasureSpec)
+        val height = View.MeasureSpec.getSize(heightMeasureSpec)
+        if (height > width) {
+            setMeasuredDimension(width, width)
+        } else {
+            setMeasuredDimension(height, height)
+        }
+    }
+
+}
