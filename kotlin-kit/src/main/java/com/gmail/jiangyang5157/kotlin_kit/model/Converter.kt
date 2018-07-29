e68c627bbe7646f780ce403896a5718a53ca0277
yangjiang <jiangyang5157@gmail.com>
2018-07-29 15:55:38 +1200

    add Converter

---
+++ kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/model/Converter.kt
@@ -0,0 +1,10 @@
+package com.gmail.jiangyang5157.kotlin_kit.model
+
+/**
+ * Created by Yang Jiang on July 29, 2018
+ */
+interface Converter<in From, in To> {
+
+    fun convert(src: From, dst: To)
+
+}
