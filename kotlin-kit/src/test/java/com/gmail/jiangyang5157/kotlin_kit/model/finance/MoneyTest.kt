c5fdbb828dbafede0983c7e6761fdd7fb8049928
yangjiang <jiangyang5157@gmail.com>
2019-01-23 00:25:15 +1300

    MoneyTest - TODO

---
+++ kotlin-kit/src/test/java/com/gmail/jiangyang5157/kotlin_kit/model/finance/MoneyTest.kt
@@ -0,0 +1,16 @@
+package com.gmail.jiangyang5157.kotlin_kit.model.finance
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+/**
+ * Created by Yang Jiang on January 23, 2019
+ */
+class MoneyTest {
+
+    @Test
+    fun test_constructor() {
+        assertTrue { false }
+    }
+
+}
