f2381aa94c467d1459be75e9110746c7d9be63a3
yangjiang <jiangyang5157@gmail.com>
2019-05-20 00:15:59 +1200

    + migrate Androidx：viewpage2 mediator

--- app/src/main/java/com/gmail/jiangyang5157/kotlin/MainActivity.kt
+++ app/src/main/java/com/gmail/jiangyang5157/kotlin/MainActivity.kt
@@ -11,7 +11,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.btn).setOnClickListener {
-            //            "btn".toast
+
         }
     }
+
 }
