468d50a606088cea138304774da84364437b85de
Yang Jiang <jiangyang5157@gmail.com>
2019-11-04 16:31:42 +1300

    refactor

--- app/src/main/java/com/gmail/jiangyang5157/kotlin/net/NetworkModule.kt
+++ app/src/main/java/com/gmail/jiangyang5157/kotlin/net/NetworkModule.kt
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull
 import com.gmail.jiangyang5157.architecture.net.LiveDataCallAdapterFactory
 import com.gmail.jiangyang5157.kotlin.net.color.ColorService
 import com.gmail.jiangyang5157.kotlin.net.color.ColorServiceInterceptor
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 /**
