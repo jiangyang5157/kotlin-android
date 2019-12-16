3ce90ef64bdc3cb7a67be2a30db38f072b3dc4ff
Yang Jiang <jiangyang5157@gmail.com>
2019-12-16 15:33:09 +1300

    TODO

--- router-annotation/src/main/java/com/gmail/jiangyang5157/router_annotation/MyClass.java
+++
@@ -1,4 +0,0 @@
-package com.gmail.jiangyang5157.router_annotation;
-
-public class MyClass {
-}
---
+++ router-annotation/src/main/java/com/gmail/jiangyang5157/router_annotation/annotation/Route.java
@@ -0,0 +1,15 @@
+package com.gmail.jiangyang5157.router_annotation.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.CLASS)
+public @interface Route {
+
+    String name() default "";
+
+    String path();
+}
