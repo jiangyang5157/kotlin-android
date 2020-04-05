029dc729d6aa28ff38faea69574e2bac682b0b51
Yang Jiang <jiangyang5157@gmail.com>
2020-04-06 11:06:07 +1200

    widget builder

--- core/src/main/java/com/gmail/jiangyang5157/core/presentation/widget/Widget.kt
+++ core/src/main/java/com/gmail/jiangyang5157/core/presentation/widget/Widget.kt
@@ -1,9 +1,16 @@
 package com.gmail.jiangyang5157.core.presentation.widget
 
+import android.content.Context
 import android.view.View
+import androidx.annotation.NonNull
 
 interface Widget {
 
     fun view(): View
+
+    interface Builder<T : Widget> {
+
+        fun build(@NonNull context: Context): T
+    }
 }
 
--- core/src/main/java/com/gmail/jiangyang5157/core/presentation/widget/WidgetBuilder.kt
+++
@@ -1,9 +0,0 @@
-package com.gmail.jiangyang5157.core.presentation.widget
-
-import android.content.Context
-import androidx.annotation.NonNull
-
-interface WidgetBuilder<T : Widget> {
-
-    fun build(@NonNull context: Context): T
-}
