a776b2c5b41e3d29955cc07ffd0f67a1d5586a92
Yang Jiang <yang.jiang@fiserv.com>
2020-05-13 17:28:50 +1200

    router example updates

--- router/src/main/java/com/gmail/jiangyang5157/android/router/core/ElementImpl.kt
+++ router/src/main/java/com/gmail/jiangyang5157/android/router/core/ElementImpl.kt
@@ -1,6 +1,6 @@
 package com.gmail.jiangyang5157.android.router.core
 
-internal data class ElementImpl<T : Route>(
+data class ElementImpl<T : Route>(
     override val route: T,
     override val key: Key = Key()
 ) : RoutingStack.Element<T>()
--- router/src/main/java/com/gmail/jiangyang5157/android/router/core/KeyImpl.kt
+++ router/src/main/java/com/gmail/jiangyang5157/android/router/core/KeyImpl.kt
@@ -3,4 +3,4 @@ package com.gmail.jiangyang5157.android.router.core
 /**
  * Default implementation of [Key] which allows a pre-defined [Key] value.
  */
-internal data class KeyImpl(override val value: String) : Key()
+data class KeyImpl(override val value: String) : Key()
