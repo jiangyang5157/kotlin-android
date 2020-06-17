9fe3de03d8881e5a8dea3063c2e757008f44fffa
Yang Jiang <yang.jiang@fiserv.com>
2020-06-17 15:52:53 +1200

    na

--- kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/data/model/Converter.kt
+++ kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/data/model/Converter.kt
@@ -1,9 +1,8 @@
 package com.gmail.jiangyang5157.kotlin_kit.data.model
 
-/**
- * Created by Yang Jiang on July 29, 2018
- */
-interface Converter<in From, in To> {
+interface Converter<A, B> {
 
-    fun convert(src: From, dst: To)
+    fun forward(a: A): B
+
+    fun backward(b: B): A
 }
--- kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/data/model/Mapper.kt
+++
@@ -1,9 +0,0 @@
-package com.gmail.jiangyang5157.kotlin_kit.data.model
-
-/**
- * Created by Yang Jiang on April 18, 2018
- */
-interface Mapper<in From, out To> {
-
-    fun map(from: From): To
-}
