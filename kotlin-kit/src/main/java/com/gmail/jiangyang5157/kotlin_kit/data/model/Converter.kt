8999f78d34d3f0f0269bc124d795ae0f2372dc65
Yang Jiang <yang.jiang@fiserv.com>
2020-06-17 16:03:26 +1200

    na

--- kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/data/model/Converter.kt
+++ kotlin-kit/src/main/java/com/gmail/jiangyang5157/kotlin_kit/data/model/Converter.kt
@@ -2,7 +2,7 @@ package com.gmail.jiangyang5157.kotlin_kit.data.model
 
 interface Converter<A, B> {
 
-    fun forward(a: A): B
+    fun forward(a: A?): B?
 
-    fun backward(b: B): A
+    fun backward(b: B?): A?
 }
